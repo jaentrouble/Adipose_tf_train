2c01e2e1487c1166540881672ec2b0456614a019
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-01 09:07:58 +0900

    sanity_check

--- sanity_check.py
+++ sanity_check.py
@@ -7,6 +7,7 @@ import datetime
 import time
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 
 inputs = keras.Input((200,200,3))
 mymodel = sanity_conv(inputs)
@@ -17,17 +18,26 @@ mymodel.compile(
     )
 mymodel.summary()
 log_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "logs/fit/" + log_name
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
                                                       histogram_freq=1,
                                                       profile_batch='3,5')
 
-
-Trash_X = np.random.randint(0,256,(1000,200,200,3), dtype=np.uint8)
-Trash_Y = np.random.randint(0,2,(1000,100,100), dtype=np.float32)
+ds = load_dataset('train_image',1000)
+Trash = ds.take(30)
 mymodel.fit(
-    x=Trash_X,
-    y=Trash_Y,
-    epochs=10,
+    x=Trash,
+
+    epochs=1,
     # steps_per_epoch=int(args.steps),
     callbacks=[tensorboard_callback],
 )
+for img, msk in ds.take(2).as_numpy_iterator():
+    fig = plt.figure(figsize=(18,16))
+    ax = fig.add_subplot(1,3,1)
+    ax.imshow(img[0])
+    ax = fig.add_subplot(1,3,2)
+    ax.imshow(msk[0], cmap='binary')
+    ax = fig.add_subplot(1,3,3)
+    ax.imshow(mymodel.predict(img), cmap='binary')
+    plt.savefig('test')
