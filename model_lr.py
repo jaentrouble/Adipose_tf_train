31e62a1378421c0510eb5b20c317e1c5aa9d061d
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-06 01:56:55 +0900

    lrstep2

--- model_lr.py
+++ model_lr.py
@@ -1,4 +1,14 @@
 def lr_step(epoch, lr):
+    lr = 0.2
+    if epoch > 10 :
+        lr = 0.02
+    if epoch > 20 :
+        lr = 0.01
+    if epoch > 50 :
+        lr = 0.005
+    return lr
+
+def lr_step2(epoch, lr):
     if epoch <= 10 :
         lr = (epoch +1) * 0.01
     if epoch > 10 :
