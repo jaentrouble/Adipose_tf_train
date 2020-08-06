d3638ebdd00202f5ed3e6390976293f657819694
Chosenjing <jaentrouble@gmail.com>
2020-08-07 01:09:20 +0900

    checked

--- model_lr.py
+++ model_lr.py
@@ -22,6 +22,17 @@ def lr_step2(epoch, lr):
         lr = 0.005
     return lr
 
+def lr_step3(epoch, lr):
+    if epoch <= 10 :
+        lr = (epoch +1) * 0.01
+    if epoch > 10 :
+        lr = 0.02
+    if epoch > 20 :
+        lr = 0.01
+    if epoch > 50 :
+        lr = lr * 0.95
+    return lr
+
 def lr_mul_inv(epoch, lr):
     return 0.01 / (epoch+1)
 
